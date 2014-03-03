3889940c648857ad90ed8236083d36d6098c6d43
blaine <blaine@blainegarrett.com>
2014-03-03 10:52:05 -0600

    Upgrading blog software. Upgrading auth system. Minor content edits.

--- settings.py
+++ settings.py
@@ -24,11 +24,29 @@ DEBUG = False
 TEMPLATE_DIRS += (os.path.join(PROJECT_DIR, 'templates'), )
 INSTALLED_APPS = ('merkabah', 'plugins.blog', 'home') # Note: Plugins need to appear first here
 
+DEFAULT_GS_BUCKET_NAME = 'blaine-garrett'
+
 APPEND_SLASH = True
 
 MIDDLEWARE_CLASSES = (
+    'gaesessions.DjangoSessionMiddleware', # located in merkabah.lib - MUST BE FIRST
     'django.middleware.common.CommonMiddleware',
+    'merkabah.core.auth.middleware.AuthenticationMiddleware',
+
+    #'django.contrib.sessions.middleware.SessionMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+###############################
+# Session and Auth Settings
+###############################
+# Note if the AUTHENTICATION_BACKENDS are not defined, it might try to use default django ones
+#    wich result in the confusing error: ImproperlyConfigured: You haven't set the database ENGINE setting yet.
+COOKIE_KEY = '12345678901234x678901234567x901234567890' #os.urandom(64)
+AUTHENTICATION_BACKENDS = (
+    'merkabah.core.auth.services.local_password.backends.LocalPasswordAuthenticationBackend',
 )
 
 
@@ -37,6 +55,7 @@ MIDDLEWARE_CLASSES = (
 ###############################
 INSTALLED_PLUGINS = ('artwork', 'blog')
 PLUGIN_PATH = 'plugins'
+plugin_settings = {}
 
 ###############################
 # Installation Properties
@@ -49,7 +68,13 @@ MERKABAH_PATH = 'merkabah/' # Used for template loaders, etc
 ###############################
 #TODO: Make this DS driven?
 o0 = {'title' :'Dashboard', 'icon' : 'icon-home', 'link' : '/', 'sub_items': []}
-o1 = {'title' :'Blog', 'icon' : 'icon-book', 'link' : '/plugin/blog', 'sub_items': []}
+o1 = {'title' :'Blog', 'icon' : 'icon-book', 'link' : '/plugin/blog/',
+    'sub_items': [
+        {'title' :'Dashboard', 'icon' : 'icon-dashboard', 'link' : '/plugin/blog/'}, 
+        {'title' :'Posts', 'icon' : 'icon-book', 'link' : '/plugin/blog/posts/'}, 
+        {'title' :'Categories', 'icon' : 'icon-book', 'link' : '/plugin/blog/categories/'}, 
+        {'title' :'Images', 'icon' : 'icon-book', 'link' : '/plugin/blog/images/'},
+    ]}
 ADMIN_PRIMARY_MENU = [o0, o1]
 
 ###############################
